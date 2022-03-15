5f1b2deaac09affce64c4ffd1b9fa0b726f838e8
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-03-15 10:14:21 +0100

    Add existing HTML and CSS from old project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="mainDiv">
+      <h1>To Do for You</h1>
+      <h2 id="msgHTML">Lägg till någonting!</h2>
+      <div>
+        <input type="text" id="inputHTML" placeholder="Skriv" />
+        <button type="submit" id="btnOK">
+          OK
+        </button>
+        <button type="submit" id="btnAdd" title="Lägg till">
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button type="submit" className="tooltip" id="btnClear">
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <div>
+          <ul className="toDoList"></ul>
+        </div>
+      </div>
     </div>
   );
 }
